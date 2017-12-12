30550d4f9972eef47c5f43b1878eca059972696c
benoitgallet <benoit.gallet@live.fr>
2017-12-12 19:11:42 +0100

    ajout d'une toolbar et d'un bouton pour faire des trucs avec plus tard (genre afficher des options)

--- app/src/main/java/com/sig/galherret/structuresjudiciairessig/MainActivity.java
+++ app/src/main/java/com/sig/galherret/structuresjudiciairessig/MainActivity.java
@@ -1,20 +1,16 @@
 package com.sig.galherret.structuresjudiciairessig;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.sig.galherret.structuresjudiciairessig.builder.HtmlBuilder;
-
-import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        manageToolbar();
 
         WebView webView = (WebView) findViewById(R.id.webView);
         webView.getSettings().setJavaScriptEnabled(true);
@@ -37,5 +35,12 @@ public class MainActivity extends AppCompatActivity {
         webView.loadUrl("javascript:showAnnuaireLieuxJustice()");
         //webView.loadUrl("file:///android_asset/mobileTest.html");
 
+    }
+
+    public void manageToolbar(){
+        Button button = findViewById(R.id.buttonShowMore);
+        button.setOnClickListener(l -> {
+            Toast.makeText(this, "afficher des trucs", Toast.LENGTH_LONG).show();
+        });
     }
 }
